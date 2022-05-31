a2e1c367b5221de7c059d4c4ab9361183f8cbb19
Jose Luis Higuera Caraveo <jose.luis.higuera@hotmail.com>
2022-05-31 10:39:18 -0600

    [FIX] STR error

--- utilities/setter.py
+++ utilities/setter.py
@@ -8,7 +8,7 @@ def main():
     """
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
     with open('path.json', 'w') as json_file:
-        json_file.write(b64decode(key.decode()))
+        json_file.write(b64decode(key).decode())
     print(os.path.realpath('path.json'))
 
 
